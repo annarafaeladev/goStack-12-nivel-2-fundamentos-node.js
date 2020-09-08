6e4f50b23ddf1e8d7eff556e3554c2c0b6751cfe
rafaela <rafabispo53@gmail.com>
2020-09-08 20:59:15 -0300

    service implementation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: CreateTransaction): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type !== 'income' && type !== 'outcome')
+      throw new Error('Tipo de transação inválida');
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('Saldo insuficiente ');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
